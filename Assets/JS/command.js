6d60b1eeaf4af9b8188c3fe45b9117f500fff350
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-09 02:24:35 +0400

    command

---
+++ Assets/JS/command.js
@@ -0,0 +1,12 @@
+help = [
+    "<br>",
+    "Wow, I thought the only people who would visit this site would be bots and spammers, guess I was wrong. <br> Just type any of the commands below to get some more info.",
+    "<br>",
+    'whoami         Who is Sushil Sharma',
+    'education      View education history',
+    'skills         View tech stack',
+    'contact        get in touch',
+    'date           Display time',
+    'clear          reset the page',
+    "<br>",
+];
