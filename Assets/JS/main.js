143d1e5b33eaf893e88ab577376e318560fab664
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-14 23:31:56 +0400

    swtich case

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,1 +1,23 @@
 var terminal = document.getElementById("terminal");
+
+
+switch (terminal) {
+  case "help":
+    console.log("help");
+    break;
+  case "whoami":
+    console.log("whoami");
+    break;
+  case "education":
+    console.log("education");
+    break;
+  case "skills":
+    console.log("skills");
+    break;
+  case "contact":
+    console.log("contact");
+    break;
+  default:
+    console.log("Command not found");
+    break;
+} 
