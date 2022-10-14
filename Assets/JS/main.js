0379a956728ade1b365e658227118639e9bcb1b3
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-15 01:01:50 +0400

    bashing

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,23 +1,30 @@
-var terminal = document.getElementById("terminal");
+function switchfunction() {
 
-
-switch (terminal) {
-  case "help":
-    console.log("help");
-    break;
-  case "whoami":
-    console.log("whoami");
-    break;
-  case "education":
-    console.log("education");
-    break;
-  case "skills":
-    console.log("skills");
-    break;
-  case "contact":
-    console.log("contact");
-    break;
-  default:
-    console.log("Command not found");
-    break;
-} 
+    var terminal = document.getElementById("terminal").value;
+    var text = "";
+    
+    switch (terminal) {
+      case "?":
+      case "help":
+        console.log("help");
+        text = "help info";
+        break;
+      case "whoami":
+        console.log("whoami");
+        break;
+      case "education":
+        console.log("education");
+        break;
+      case "skills":
+        console.log("skills");
+        break;
+      case "contact":
+        console.log("contact");
+        break;
+      default:
+        console.log("Command not found");
+        break;
+    }
+    document.getElementById("output").innerHTML = text;
+    
+    }
