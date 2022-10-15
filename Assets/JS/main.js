3cf8195046b81cca71d6acf7426ca7e4266f10c6
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-16 02:49:56 +0400

    coding

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,36 +1,36 @@
 function switchfunction() {
 
-    var terminal = document.getElementById("terminal").value;
-    var text = "";
-    
-    switch (terminal) {
-      case "?":
-      case "help":
-        console.log("help");
-        text = "help info";
-        break;
-      case "whoami":
-        console.log("whoami");
-        text = "whoami info";
-        break;
-      case "education":
-        console.log("education");
-        text = "education info";
-        break;
-      case "skills":
-        console.log("skills");
-        text = "skills info";
-        break;
-      case "contact":
-        console.log("contact");
-        text = "contact info";
-        break;
-      default:
-        console.log("Command not found");
-        text = "Command not found";
-        break;
-    }
+  var terminal = document.querySelector('#terminal').value.trim().toLowerCase();
+  var text = "";
 
-    document.getElementById("output").innerHTML = text;
+  switch (terminal) {
+    case "?":
+    case "help":
+      console.log("help");
+      text = "help info";
+      break;
+    case "whoami":
+      console.log("whoami");
+      text = "whoami info";
+      break;
+    case "education":
+      console.log("education");
+      text = "education info";
+      break;
+    case "skills":
+      console.log("skills");
+      text = "skills info";
+      break;
+    case "contact":
+      console.log("contact");
+      text = "contact info";
+      break;
+    default:
+      console.log("Command not found");
+      text = "Command not found";
+      break;
+  }
 
-    }
+  document.getElementById("output").innerHTML = text;
+
+}
