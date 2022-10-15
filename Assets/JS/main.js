6090256cf88b747f42331a930bad13d51332f793
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-16 00:12:53 +0400

    description

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -11,18 +11,23 @@ function switchfunction() {
         break;
       case "whoami":
         console.log("whoami");
+        text = "whoami info";
         break;
       case "education":
         console.log("education");
+        text = "education info";
         break;
       case "skills":
         console.log("skills");
+        text = "skills info";
         break;
       case "contact":
         console.log("contact");
+        text = "contact info";
         break;
       default:
         console.log("Command not found");
+        text = "Command not found";
         break;
     }
     document.getElementById("output").innerHTML = text;
