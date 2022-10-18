1219bb7ab6333bf4b8396ec10d35336e950342d0
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-19 00:20:33 +0400

    commands

--- Assets/JS/command.js
+++ Assets/JS/command.js
@@ -45,11 +45,14 @@ var contact = [
 ].join('<br>');
 
 var banner = [
+    '<span class="index">TheSushilSharma (SS) Not A Corporation. All rights reserved.</span>',
     "<pre>",
     "(~    _|_ .|  (~|_  _  _ _ _  _ ",
     "_)|_|_\| |||  _)| |(_|| | | |(_|",
 
-    "</pre>"
+    "</pre>",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
 ].join('<br>');
 
 var invalid = [
