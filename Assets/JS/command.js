45a0469a1d6eb3d4eb576f2ed91527b5d37f8abd
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-10-18 23:50:21 +0400

    commands

--- Assets/JS/command.js
+++ Assets/JS/command.js
@@ -1,39 +1,30 @@
-help = [
+var help = [
     "<br>",
     "Wow, I thought the only people who would visit this site would be bots and spammers, guess I was wrong. <br> Just type any of the commands below to get some more info.",
     "<br>",
-    'whoami         Who is Sushil Sharma',
-    'education      View education history',
-    'skills         View tech stack',
-    'contact        get in touch',
-    'date           Display time',
-    'clear          reset the page',
+    'whoami - Who is Sushil Sharma',
+    'education - View education history',
+    'skills - View tech stack',
+    'contact - get in touch',
+    'date - Display time',
+    'clear - reset the page',
     "<br>",
-];
+].join('<br>');
 
-banner = [
-    "<pre>",
-    "(~    _|_ .|  (~|_  _  _ _ _  _ ",
-    "_)|_|_\| |||  _)| |(_|| | | |(_|",
-
-    "</pre>"
-
-];
-
-whoami = [
+var whoami = [
     "<br>",
     "Sushil Sharma",
     "I have expertise designing, developing, and implementing apps and solutions using a variety of technologies and programming languages. ",
     "<br>"
-];
+].join('<br>');
 
-education = [
+var education = [
     "<br>",
     "Bachelor of Computer",
     "<br>"
-];
+].join('<br>');
 
-skills = [
+var skills = [
     "<br>",
     "CPP",
     "Java",
@@ -42,11 +33,21 @@ skills = [
     "HTML5",
     "CSS3",
     "<br>"
-];
+].join('<br>');
 
-contact = [
+var contact = [
     "<br>",
-    "Github",
-    "Linkdein",
+    'You can contact me on any of the following links:<br>',
+    '"<a target="_blank" rel="noopener noreferrer" href="https://github.com/thesushilsharma">GitHub</a>"',
+    '"<a target="_blank" rel="noopener noreferrer" href="https://medium.com/@thesushilsharma">Medium</a>"',
+    '"<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/thesushilsharma">Linkedin</a>"',
     "<br>"
-];
+].join('<br>');
+
+var banner = [
+    "<pre>",
+    "(~    _|_ .|  (~|_  _  _ _ _  _ ",
+    "_)|_|_\| |||  _)| |(_|| | | |(_|",
+
+    "</pre>"
+].join('<br>');
