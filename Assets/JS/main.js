55f533bb9c450fd09732f59838b3461021b48390
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2023-03-14 19:56:21 +0400

    add input code to display

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -5,49 +5,50 @@ function handleCommand() {
 
   switch (command) {
     case "?":
-      case "help":
-        console.log("help");
-        // text = "help info";
-        commands(help);
-        break;
-      case "whoami":
-        console.log("whoami");
-        commands(whoami);
-        //text = "whoami info";
-        break;
-      case "education":
-        console.log("education");
-        commands(education);
-        //text = "education info";
-        break;
-      case "skills":
-        console.log("skills");
-        commands(skills);
-        //text = "skills info";
-        break;
-      case "contact":
-        console.log("contact");
-        commands(contact);
-        //text = "contact info";
-        break;
-      case "clear":
-        console.log("clear");
-        document.getElementById("output").innerHTML = "";
-        //commands(clear);
-        commands(banner);
-        //text = "clear info";
-        break;
-      default:
-        console.log("Command not found");
-        commands(invalid);
-        // text = "Command not found";
-        break;
+    case "help":
+      console.log("help");
+      // text = "help info";
+      commands(help);
+      break;
+    case "whoami":
+      console.log("whoami");
+      commands(whoami);
+      //text = "whoami info";
+      break;
+    case "education":
+      console.log("education");
+      commands(education);
+      //text = "education info";
+      break;
+    case "skills":
+      console.log("skills");
+      commands(skills);
+      //text = "skills info";
+      break;
+    case "contact":
+      console.log("contact");
+      commands(contact);
+      //text = "contact info";
+      break;
+    case "clear":
+      console.log("clear");
+      outputElement.innerHTML = '';
+      //commands(clear);
+      commands(banner);
+      //text = "clear info";
+      break;
+    default:
+      console.log("Command not found");
+      commands(invalid);
+      // text = "Command not found";
+      break;
   }
 
   function commands(commandoutput) {
+     outputElement.innerHTML += '<p class="text-red-500">visitor@thesushilsharma:~$; ' + inputElement.value + '</p>';
     outputElement.innerHTML += '<p>' + commandoutput + '</p>';
     // Clear input field
     inputElement.value = '';
   }
-
+  
 }
