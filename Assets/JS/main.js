61f48f204b7dc48ffc3bd846e679217191d8e689
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2023-03-15 02:35:52 +0400

    dating more fixing to input tag

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,64 +1,75 @@
-function handleCommand() {
-  const inputElement = document.querySelector('#terminal-input');
-  const outputElement = document.querySelector('#terminal-output');
-  const command = inputElement.value.trim().toLowerCase();
+const outputElement = document.querySelector('#terminal-output');
+const inputElement = document.querySelector('#terminal-input');
 
-  switch (command) {
-    case "?":
-    case "help":
-      console.log("help");
-      // text = "help info";
-      commands(help);
-      break;
-    case "whoami":
-      console.log("whoami");
-      commands(whoami);
-      //text = "whoami info";
-      break;
-    case "education":
-      console.log("education");
-      commands(education);
-      //text = "education info";
-      break;
-    case "skills":
-      console.log("skills");
-      commands(skills);
-      //text = "skills info";
-      break;
-    case "contact":
-      console.log("contact");
-      commands(contact);
-      //text = "contact info";
-      break;
-    case "banner":
-      console.log("banner");
-      commands(banner);
-      //text = "banner";
-      break;
-    case "date":
-      console.log("date");
-      commands(date);
-      //text = "date info";
-      break;
-    case "clear":
-      console.log("clear");
-      outputElement.innerHTML = '';
-      commands(banner);
-      //text = "clear info";
-      break;
-    default:
-      console.log("Command not found");
-      commands(invalid);
-      // text = "Command not found";
-      break;
-  }
-
-  function commands(commandoutput) {
-    outputElement.innerHTML += '<p class="text-red-500">visitor@thesushilsharma:~$; ' + inputElement.value + '</p>';
-    outputElement.innerHTML += '<p>' + commandoutput + '</p>';
-    // Clear input field
-    inputElement.value = '';
-    window.scrollTo(0, document.body.offsetHeight);
-  }
-
+function commands(commandoutput) {
+  outputElement.innerHTML += '<p class="text-red-500">visitor@thesushilsharma:~$; ' + inputElement.value + '</p>';
+  outputElement.innerHTML += '<p>' + commandoutput + '</p>';
+  // Clear input field
+  inputElement.value = '';
+  window.scrollTo(0, document.documentElement.scrollHeight);
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  commands(banner);
+
+  function handleCommand() {
+
+    const command = inputElement.value.trim().toLowerCase();
+
+    switch (command) {
+      case "?":
+      case "help":
+        console.log("help");
+        // text = "help info";
+        commands(help);
+        break;
+      case "whoami":
+        console.log("whoami");
+        commands(whoami);
+        //text = "whoami info";
+        break;
+      case "education":
+        console.log("education");
+        commands(education);
+        //text = "education info";
+        break;
+      case "skills":
+        console.log("skills");
+        commands(skills);
+        //text = "skills info";
+        break;
+      case "contact":
+        console.log("contact");
+        commands(contact);
+        //text = "contact info";
+        break;
+      case "banner":
+        console.log("banner");
+        commands(banner);
+        //text = "banner";
+        break;
+      case "date":
+        console.log("date");
+        commands(date);
+        //text = "date info";
+        break;
+      case "clear":
+        console.log("clear");
+        outputElement.innerHTML = '';
+        commands(banner);
+        //text = "clear info";
+        break;
+      default:
+        console.log("Command not found");
+        commands(invalid);
+        // text = "Command not found";
+        break;
+    }
+  }
+
+  document.querySelector('#terminal-input').addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      handleCommand();
+    }
+  });
+});
